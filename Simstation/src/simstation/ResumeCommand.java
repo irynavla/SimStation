40bf4bd79cb77fdf5da9c218bd41f366714f346e
irynavla <i.vlasiuk.04@gmail.com>
2025-04-13 15:46:54 -0700

    SimStation complete

--- Simstation/src/simstation/ResumeCommand.java
+++ Simstation/src/simstation/ResumeCommand.java
@@ -1,4 +1,18 @@
 package simstation;
 
-public class ResumeCommand {
+import mvc.Command;
+import mvc.Model;
+
+
+public class ResumeCommand extends Command {
+
+    public ResumeCommand(Model model) { //resume constructor
+        super(model); //super
+    }
+
+    @Override //Override from command
+    public void execute() { //execute method
+        Simulation simulation = (Simulation) model; //set the simulation
+        simulation.resume(); //resume the simulation
+    }
 }
