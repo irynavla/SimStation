c9c8338f46d1cc91b68c2e56c586b56a7d667fae
fatimacisneros <fatima.cisneros@sjsu.edu>
2025-04-13 07:20:37 -0700

    Stats and Start Commands

--- Simstation/src/simstation/StatsCommand.java
+++ Simstation/src/simstation/StatsCommand.java
@@ -1,4 +1,16 @@
 package simstation;
 
-public class StatsCommand {
+import mvc.*;
+
+public class StatsCommand extends Command{
+    public StatsCommand(Model model){
+        super(model);
+    }
+
+    public void execute(){
+        World world = (World) model;
+        int totalAgents = world.getAgents().size();
+        int runningAgents = world.getRunningAgents();
+        Utilities.inform("Total Agents: " + totalAgents + "\nRunning Agents: " + runningAgents);
+    }
 }
